d2979456efcc4c658c4104fa66dc1053433f870b
eggstoastbacon <eggstoastbacon@gmail.com>
2023-12-18 11:30:47 -0800

    Update to fix Editor code - Revert to 1.2.2

--- Arduinoboy/Memory_Functions.ino
+++ Arduinoboy/Memory_Functions.ino
@@ -1,28 +1,23 @@
-
 boolean checkMemory()
 {
   byte chk;
-  #ifndef USE_DUE
   for(int m=0;m<4;m++){
     chk =  EEPROM.read(MEM_CHECK+m);
     if(chk != defaultMemoryMap[MEM_CHECK+m]) {
       return false;
     }
   }
-  #endif
   return true;
 }
 
 void initMemory(boolean reinit)
 {
   if(!alwaysUseDefaultSettings) {
-    #ifndef USE_DUE
     if(reinit || !checkMemory()) {
       for(int m=(MEM_MAX);m>=0;m--){
         EEPROM.write(m,defaultMemoryMap[m]);
       }
     }
-    #endif
     loadMemory();
   } else {
     for(int m=0;m<=MEM_MAX;m++){
@@ -35,29 +30,32 @@ void initMemory(boolean reinit)
 
 void loadMemory()
 {
-  #ifndef USE_DUE
   for(int m=(MEM_MAX);m>=0;m--){
      memory[m] = EEPROM.read(m);
   }
-  #endif
   changeTasks();
 }
 
 void printMemory()
 {
   for(int m=0;m<=MEM_MAX;m++){
-    serial->println(memory[m],HEX);
+    Serial.println(memory[m],HEX);
+  }
+}
+
+void sendMemory()
+{
+  for(int m=0;m<=MEM_MAX;m++){
+    Serial.write(memory[m]);
   }
 }
 
 void saveMemory()
 {
-  #ifndef USE_DUE
   for(int m=(MEM_MAX-1);m>=0;m--){
     EEPROM.write(m,memory[m]);
   }
   changeTasks();
-  #endif
 }
 
 void changeTasks()
